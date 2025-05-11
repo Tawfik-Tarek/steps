d451b794905e38cba6450ff3f4bebc0aa08cd9bd
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2025-05-11 22:20:52 +0300

    Add progress bar to indicate current step in the navigation

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <div className={`${currentStep >= 1 ? "active" : ""}`}>1</div>
         <div className={`${currentStep >= 2 ? "active" : ""}`}>2</div>
         <div className={`${currentStep === 3 ? "active" : ""}`}>3</div>
+        <span className="progress-bar" style={{width: `${ (currentStep - 1) * 50}%` }}></span>
       </div>
       <p className="message"> {messages[currentStep - 1] || "anything"} </p>
       <div className="buttons">
